40c169e2a092addf3f534893ed874b01ab7c23e2
sahilalam <alamsahil939@gmail.com>
2021-03-27 22:55:50 +0530

    updated

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ app.post('/register',async(req,res)=>{
         else
         {
            const encrypted_mail=base64.encode(email);
-           const href=`"${process.env.FRONT_URL}/login/${encrypted_mail}"`;
-           let html=`<a href="${href}">Click Here</a> to verify your E-Mail`;
+           const href=`${process.env.FRONT_URL}/login/${encrypted_mail}`;
+           let html=`<a href=${href}>Click Here</a> to verify your E-Mail`;
            
            let info =await transporter.sendMail({
                from:"Sahil Alam",
